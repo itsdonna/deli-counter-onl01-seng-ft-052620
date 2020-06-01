f736398829bc14e196b22144f95e96818e4909a8
Donna Torres <todonnatorres@icloud.com>
2020-06-01 06:01:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,8 @@ end
 def now_serving(array, name)
 
   if array.length > 0 
-  puts "Currently serving #{array.shift}."
-else
-  puts "There is nobody waiting to be served!"
-end 
+    puts "Currently serving #{array.shift}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
 end
